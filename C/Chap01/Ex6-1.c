ecbd11fb1572f6e240f1e72df67149e673d0d6d1
peasporridgecold <88437889+peasporridgecold@users.noreply.github.com>
2025-11-03 11:55:54 +0900

    Ex6-1 문제
    1~10 출력하는 whie

---
+++ C/Chap01/Ex6-1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	//1~10까지 출력하는 while을 작성
+	int i = 1;
+
+	while (i < 11) {
+		printf("i : %d\n", i);
+		i++;
+	}
+	return 0;
+}
